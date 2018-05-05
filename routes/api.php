924e194c206f3548eb337d2d5dc74146d53a4090
VZ <vitaly.zibulski@gmail.com>
2018-05-05 23:13:55 +0300

    add route

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/tasks', 'TaskController', [
 
-    'except'=> ['edit', 'show']
-
+Route::resource('/tasks', 'TaskController',[
+	'except' => ['edit','show','']
 ]);
